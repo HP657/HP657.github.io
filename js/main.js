8b301e8e401adbd5d1fe5e23a09e45a4a5a4408d
HP657 <junlee5070@gmail.com>
2024-02-22 21:06:08 +0900

    돌아가기 버튼 컨텐트에서 뺴옴 ㅋㅋ

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ import { addScheduleItem } from './addSchedule.js';
 
 document.addEventListener("DOMContentLoaded", function() {
     // 새로운 스케쥴 항목 추가
+//  addScheduleItem("MMDD", "일정");
     addScheduleItem("0304", "개학식");
-    addScheduleItem("0305", "");
 
 
 
