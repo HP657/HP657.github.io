b6649bceee4bbc0f34bf8fb985150420669b2b86
HP657 <junlee5070@gmail.com>
2024-03-05 20:36:38 +0900

    일정 추가

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     // 새로운 스케쥴 항목 추가
 //  addScheduleItem("MMDD", "일정");
     addScheduleItem("0304", "개학식");
+    addScheduleItem("0306", "졸업사진촬영날");
 
 
 
