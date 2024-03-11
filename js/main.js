2d51a2e7366401c4c4a029e53a5a81c1cf91ccda
HP657 <junlee5070@gmail.com>
2024-03-11 22:48:06 +0900

    매우 간소화

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
-import { addScheduleItem } from './addSchedule.js';
+document.addEventListener('DOMContentLoaded', function() {
+    var scheduleDiv = document.querySelector('.schedule');
 
-document.addEventListener("DOMContentLoaded", function() {
-    // 새로운 스케쥴 항목 추가
-//  addScheduleItem("MMDD", "일정");
-    addScheduleItem("0304", "개학식");
-    addScheduleItem("0306", "졸업사진촬영날");
-
-
-
+    scheduleDiv.addEventListener('click', function() {
+        var link = scheduleDiv.querySelector('a').getAttribute('href');
+        window.location.href = link;
+    });
 });
