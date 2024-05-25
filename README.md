d2941c661e40bb08f682a811a4076b0021d04429
HP657 <junlee5070@gmail.com>
2024-05-25 19:49:00 +0900

    기존 3학년 4반 정보 제공 프로젝트 철회 명함 페이지로 전환

--- README.md
+++
@@ -1,7 +0,0 @@
-# HP657.github.io
-
-무료로 사용 할수있는 내도메인.한국에서 daejin3-4.kro.kr 도메인을 연결 시켜 놓았다.
-
-http://daejin3-4.kro.kr
-
-우리 반을 위해 시간표, 급식 중요한 시간표 변경 사항이나 공지사항을 알려주기 위한 Github Page
--- js/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var scheduleDiv = document.querySelector('.schedule');
-
-    scheduleDiv.addEventListener('click', function() {
-        var link = scheduleDiv.querySelector('a').getAttribute('href');
-        window.location.href = link;
-    });
-});
