b1b77c5e8650390e8dac11d39f49fb6b8091ac0d
HP657 <junlee5070@gmail.com>
2024-06-03 00:21:12 +0900

    삭제

--- js/script.js
+++
@@ -1,9 +0,0 @@
-const cardContainer = document.querySelector('.card-container');
-
-cardContainer.addEventListener('mouseenter', () => {
-    cardContainer.querySelector('.card').style.transform = 'rotateY(180deg)';
-});
-
-cardContainer.addEventListener('mouseleave', () => {
-    cardContainer.querySelector('.card').style.transform = 'rotateY(0deg)';
-});
